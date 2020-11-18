43e66b0474d405d8ae033f70b8500cbd6ff9811d
Edgar <Edgar@AnotherFoxGuy.com>
2020-11-18 16:26:54 +0100

    :arrow_up: Update conan to 1.31.3

--- install-conan.sh
+++ install-conan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_31_1.deb" -nv -O /tmp/conan.deb
+wget "https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_31_3.deb" -nv -O /tmp/conan.deb
 dpkg -i /tmp/conan.deb
