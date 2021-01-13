18e5383df0a9450f5fe6bf28ed8be96f178f5af3
Edgar <Edgar@AnotherFoxGuy.com>
2021-01-13 12:16:30 +0100

    :arrow_up: Update sscache to 0.2.15

--- install-sccache.sh
+++ install-sccache.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/sccache
-wget "https://github.com/mozilla/sccache/releases/download/0.2.14/sccache-0.2.14-x86_64-unknown-linux-musl.tar.gz" -nv -O /tmp/sccache.tar.gz
+wget "https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz" -nv -O /tmp/sccache.tar.gz
 tar xvzf /tmp/sccache.tar.gz --strip 1 -C /tmp/sccache
 install -m 755 /tmp/sccache/sccache /usr/local/bin/
