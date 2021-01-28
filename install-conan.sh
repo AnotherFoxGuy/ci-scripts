ce00f92dcff52d33633e1635d050235836e9bf73
Edgar <Edgar@AnotherFoxGuy.com>
2021-01-28 10:50:56 +0100

    :arrow_up: Update conan and cmake

--- install-conan.sh
+++ install-conan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_32_1.deb" -nv -O /tmp/conan.deb
+wget "https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_33_0.deb" -nv -O /tmp/conan.deb
 dpkg -i /tmp/conan.deb
