cd9f3df6b7d6d1e8dc77715e64068d48de1e48dd
Edgar <Edgar@AnotherFoxGuy.com>
2021-04-02 14:55:16 +0200

    :arrow_up: Update conan

--- install-conan.sh
+++ install-conan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_34_1.deb" -nv -O /tmp/conan.deb
+wget "https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_35_0.deb" -nv -O /tmp/conan.deb
 dpkg -i /tmp/conan.deb
