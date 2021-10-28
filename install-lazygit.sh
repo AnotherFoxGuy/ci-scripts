d0d835cbb50bb8f48ac45cc3dbd143b0020af7c8
Edgar <Edgar@AnotherFoxGuy.com>
2021-10-28 13:57:57 +0200

    :arrow_up: Updated cmake, lazygit and miniserve

--- install-lazygit.sh
+++ install-lazygit.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/lazygit
-wget "https://github.com/jesseduffield/lazygit/releases/download/v0.29/lazygit_0.29_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
+wget "https://github.com/jesseduffield/lazygit/releases/download/v0.30.1/lazygit_0.30.1_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
 tar xvzf /tmp/lazygit.tar.gz -C /tmp/lazygit
 install -m 755 /tmp/lazygit/lazygit /usr/local/bin/
--- install-miniserve.sh
+++ install-miniserve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/svenstaro/miniserve/releases/download/v0.17.0/miniserve-v0.17.0-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
+wget "https://github.com/svenstaro/miniserve/releases/download/v0.18.0/miniserve-v0.18.0-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
 install -m 755 /tmp/miniserve /usr/local/bin/
