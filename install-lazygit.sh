2f463d8ef6e6ec0254937fa84c9c343ad6700b64
Edgar <Edgar@ANotherFoxGuy.com>
2021-11-08 13:42:28 +0100

    :arrow_up: Update cmake and lazygit

--- install-lazygit.sh
+++ install-lazygit.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/lazygit
-wget "https://github.com/jesseduffield/lazygit/releases/download/v0.30.1/lazygit_0.30.1_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
+wget "https://github.com/jesseduffield/lazygit/releases/download/v0.31.2/lazygit_0.31.2_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
 tar xvzf /tmp/lazygit.tar.gz -C /tmp/lazygit
 install -m 755 /tmp/lazygit/lazygit /usr/local/bin/
