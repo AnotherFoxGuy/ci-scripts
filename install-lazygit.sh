2ff27d6976933454c705963464a753a9f394da7b
Edgar <Edgar@AnotherFoxGuy.com>
2022-01-17 15:43:47 +0000

    :arrow_up: Updated lazygit

--- install-lazygit.sh
+++ install-lazygit.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/lazygit
-wget "https://github.com/jesseduffield/lazygit/releases/download/v0.32/lazygit_0.32_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
+wget "https://github.com/jesseduffield/lazygit/releases/download/v0.32.2/lazygit_0.32.2_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
 tar xvzf /tmp/lazygit.tar.gz -C /tmp/lazygit
 install -m 755 /tmp/lazygit/lazygit /usr/local/bin/
