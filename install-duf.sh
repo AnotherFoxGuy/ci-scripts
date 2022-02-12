b83a6ec43f8a8d8345685346c33a446bd8394ef8
Edgar <Edgar@AnotherFoxGuy.com>
2022-02-12 18:07:19 +0100

    :arrow_up: Updated tools

--- install-duf.sh
+++ install-duf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/muesli/duf/releases/download/v0.7.0/duf_0.7.0_linux_amd64.deb" -nv -O /tmp/duf.deb
+wget "https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.deb" -nv -O /tmp/duf.deb
 dpkg -i /tmp/duf.deb
