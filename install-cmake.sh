ff28da95fc4a7bfe40dbd94990d0ff95d63668f8
Edgar <Edgar@AnotherFoxGuy.com>
2022-02-13 19:13:52 +0100

    :bug: Fixed cmake install

--- install-cmake.sh
+++ install-cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/Kitware/CMake/releases/download/v3.23.01/cmake-3.23.01-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
+wget "https://github.com/Kitware/CMake/releases/download/v3.21.5/cmake-3.21.5-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
 sh /tmp/cmake.sh --exclude-subdir --prefix=/usr/local
