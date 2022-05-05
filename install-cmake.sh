7aa5b72532f9a0ada77b9f63484543671079d91d
Edgar <Edgar@AnotherFoxGuy.com>
2022-05-05 19:14:25 +0200

    :arrow_up: Updated tools

--- install-cmake.sh
+++ install-cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/Kitware/CMake/releases/download/v3.22.3/cmake-3.22.3-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
+wget "https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
 sh /tmp/cmake.sh --exclude-subdir --prefix=/usr/local
--- install-miniserve.sh
+++ install-miniserve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/svenstaro/miniserve/releases/download/v0.19.3/miniserve-v0.19.3-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
+wget "https://github.com/svenstaro/miniserve/releases/download/v0.19.4/miniserve-v0.19.4-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
 install -m 755 /tmp/miniserve /usr/local/bin/
