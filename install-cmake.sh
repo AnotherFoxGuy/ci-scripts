96883bc95f90934da90feba2e4bee4269c27d83e
Edgar <Edgar@AnotherFoxGuy.com>
2022-08-09 17:22:59 +0200

    :arrow_up: Updated tools

--- install-cmake.sh
+++ install-cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
+wget "https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
 sh /tmp/cmake.sh --exclude-subdir --prefix=/usr/local
