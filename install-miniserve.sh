2343769a29298db01d3fbac8aae308889858a37f
Edgar <Edgar@AnotherFoxGuy.com>
2023-03-21 11:17:16 +0100

    :arrow_up: Updated tools

--- install-miniserve.sh
+++ install-miniserve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/svenstaro/miniserve/releases/download/v0.22.0/miniserve-v0.22.0-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
+wget "https://github.com/svenstaro/miniserve/releases/download/v0.23.0/miniserve-v0.23.0-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
 install -m 755 /tmp/miniserve /usr/local/bin/
