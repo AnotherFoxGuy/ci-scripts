31340e8d1034eb22eaccf954b7254005b1249dba
Edgar <Edgar@AnotherFoxGuy.com>
2023-07-23 11:09:19 +0200

    :arrow_up: Updated tools

--- install-cmake.sh
+++ install-cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/Kitware/CMake/releases/download/v3.26.4/cmake-3.26.4-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
+wget "https://github.com/Kitware/CMake/releases/download/v3.27.0/cmake-3.27.0-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
 sh /tmp/cmake.sh --exclude-subdir --prefix=/usr/local
--- install-lazydocker.sh
+++ install-lazydocker.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/lazydocker
-wget "https://github.com/jesseduffield/lazydocker/releases/download/v0.20.0/lazydocker_0.20.0_Linux_x86_64.tar.gz" -nv -O /tmp/lazydocker.tar.gz
+wget "https://github.com/jesseduffield/lazydocker/releases/download/v0.21.0/lazydocker_0.21.0_Linux_x86_64.tar.gz" -nv -O /tmp/lazydocker.tar.gz
 tar xvzf /tmp/lazydocker.tar.gz -C /tmp/lazydocker
 install -m 755 /tmp/lazydocker/lazydocker /usr/local/bin/
