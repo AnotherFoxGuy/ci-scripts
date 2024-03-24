77f004c2a2210498f0e782a0b01c75935253d65e
Edgar <Edgar@AnotherFoxGuy.com>
2024-03-24 12:02:34 +0100

    :arrow_up: Updated tools

--- install-lazygit.sh
+++ install-lazygit.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/lazygit
-wget "https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
+wget "https://github.com/jesseduffield/lazygit/releases/download/v0.41.0/lazygit_0.41.0_Linux_x86_64.tar.gz" -nv -O /tmp/lazygit.tar.gz
 tar xvzf /tmp/lazygit.tar.gz -C /tmp/lazygit
 install -m 755 /tmp/lazygit/lazygit /usr/local/bin/
--- install-miniserve.sh
+++ install-miniserve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/svenstaro/miniserve/releases/download/v0.26.0/miniserve-v0.26.0-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
+wget "https://github.com/svenstaro/miniserve/releases/download/v0.27.1/miniserve-v0.27.1-x86_64-unknown-linux-musl" -nv -O /tmp/miniserve
 install -m 755 /tmp/miniserve /usr/local/bin/
