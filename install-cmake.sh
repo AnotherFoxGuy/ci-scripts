1aea187d9cc5c81fb8e7231600021a40b4fae99e
Edgar <Edgar@AnotherFoxGuy.com>
2024-04-28 14:18:04 +0200

    :arrow_up: Updated tools

--- install-cmake.sh
+++ install-cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/Kitware/CMake/releases/download/v3.29.0/cmake-3.29.0-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
+wget "https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2-Linux-x86_64.sh" -nv -O /tmp/cmake.sh
 sh /tmp/cmake.sh --exclude-subdir --prefix=/usr/local
--- install-sccache.sh
+++ install-sccache.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/sccache
-wget "https://github.com/mozilla/sccache/releases/download/v0.7.7/sccache-v0.7.7-x86_64-unknown-linux-musl.tar.gz" -nv -O /tmp/sccache.tar.gz
+wget "https://github.com/mozilla/sccache/releases/download/v0.8.0/sccache-v0.8.0-x86_64-unknown-linux-musl.tar.gz" -nv -O /tmp/sccache.tar.gz
 tar xvzf /tmp/sccache.tar.gz --strip 1 -C /tmp/sccache
 install -m 755 /tmp/sccache/sccache /usr/local/bin/
