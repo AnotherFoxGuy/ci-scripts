0a2e22e3822ef85fc1ac98e12219af136614fb45
Edgar <Edgar@AnotherFoxGuy.com>
2024-11-28 18:15:00 +0100

    :arrow_up: Updated tools

--- install-lazydocker.sh
+++ install-lazydocker.sh
@@ -2,6 +2,6 @@
 set -eu
 
 mkdir -p /tmp/lazydocker
-wget "https://github.com/jesseduffield/lazydocker/releases/download/v0.23.3/lazydocker_0.23.3_Linux_x86_64.tar.gz" -nv -O /tmp/lazydocker.tar.gz
+wget "https://github.com/jesseduffield/lazydocker/releases/download/v0.24.1/lazydocker_0.24.1_Linux_x86_64.tar.gz" -nv -O /tmp/lazydocker.tar.gz
 tar xvzf /tmp/lazydocker.tar.gz -C /tmp/lazydocker
 install -m 755 /tmp/lazydocker/lazydocker /usr/local/bin/
