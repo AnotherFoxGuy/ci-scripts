7f6790fe8402309e855ca613f5350bb2d15b292a
Edgar <Edgar@AnotherFoxGuy.com>
2025-02-06 10:32:10 +0100

    :arrow_up: Updated tools

--- install-gitea.sh
+++ install-gitea.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-wget "https://github.com/go-gitea/gitea/releases/download/v1.22.6/gitea-1.22.6-linux-amd64" -nv -O /tmp/gitea
+wget "https://github.com/go-gitea/gitea/releases/download/v1.23.3/gitea-1.23.3-linux-amd64" -nv -O /tmp/gitea
 install -m 755 /tmp/gitea /usr/local/bin/
