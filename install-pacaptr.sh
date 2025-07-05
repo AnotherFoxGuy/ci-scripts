908b3dfab5f8f7ccacd26c1129be78c7f253ca71
Edgar <Edgar@AnotherFoxGuy.com>
2025-07-05 10:18:55 +0200

    :wrench: Rename pacaptr to yay

--- install-pacaptr.sh
+++ install-pacaptr.sh
@@ -4,4 +4,4 @@ set -eu
 mkdir -p /tmp/pacaptr
 wget "https://github.com/rami3l/pacaptr/releases/download/v0.23.0/pacaptr-linux-amd64.tar.gz" -nv -O /tmp/pacaptr.tar.gz
 tar xvzf /tmp/pacaptr.tar.gz -C /tmp/pacaptr
-install -m 755 /tmp/pacaptr/pacaptr /usr/local/bin/fakeman
+install -m 755 /tmp/pacaptr/pacaptr /usr/local/bin/yay
